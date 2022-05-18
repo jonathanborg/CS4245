7b01b4f36e59376ae164acae5d14f19fdc462e5e
Jonathan Borg <jonathanborg1996@gmail.com>
2022-05-18 22:52:10 +0200

    Added missing package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 	packages=['experiments', 'models', 'utils'],
 	author='Group 24',
 	description='CS4245_Project',
-	install_requires=['tqdm==4.64', 'numpy==1.22.3', 'torch==1.11.0', 'torchvision==0.12.0', 'scipy==1.8.1'],
+	install_requires=['tqdm==4.64', 'numpy==1.22.3', 'torchvision==0.12.0', 'scipy==1.8.1', 'matplotlib==3.5.2'],
 	classifiers=[
 		'Programming Language :: Python :: 3.9',
 	],
